829ac7d005478e7892dde893ac5ec777431c29ac
Christian Bühler <christian@cbuehler.de>
2010-08-11 23:49:54 +0200

    Initial commit

---
+++ src/Minimal.java
@@ -0,0 +1,14 @@
+import javax.microedition.midlet.*;
+
+public class Minimal extends MIDlet {
+
+	public void destroyApp(boolean unconditional) {
+	}
+
+	public void pauseApp() {
+	}
+
+	public void startApp() {
+	}
+
+}
