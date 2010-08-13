62c306f92cc5dc1b1120d1838823bfb8e6c34bae
Christian Bühler <christian@cbuehler.de>
2010-08-13 23:52:16 +0200

    rudimentary gpsd implementation for cachewolf (not working yet)

--- src/GPSd.java
+++ src/GPSd.java
@@ -1,5 +1,8 @@
+import java.io.*;
+import java.util.*;
 import javax.microedition.midlet.*;
 import javax.microedition.io.*;
+import javax.microedition.lcdui.*;
 
 public class GPSd extends MIDlet {
 
@@ -10,6 +13,25 @@ public class GPSd extends MIDlet {
 	}
 
 	public void startApp() {
+		try {
+			ServerSocketConnection scn = (ServerSocketConnection)Connector.open("socket://:2947");
+			StreamConnection sc = scn.acceptAndOpen();
+			OutputStream os = sc.openOutputStream();
+			String s;
+
+			s = "{\"class\":\"VERSION\",\"proto_major\":3}";
+			os.write(s.getBytes());
+
+			long timestamp = new Date().getTime();
+			s = "{\"class\":\"POLL\",\"timestamp\":"+timestamp/1000.0+",\"fixes\":|{\"mode\":0}|,\"skyviews\":||}";
+			os.write(s.getBytes());
+
+			os.close();
+			sc.close();
+			scn.close();
+		} catch (IOException e) {
+			Display.getDisplay(this).setCurrent(new Alert(e.getMessage()));
+		}
 	}
 
 }
